3747ab208ab00ace23ed5213db5d26be5776ef0d
Garrett Hoofman <garrett@th3rdcoast.com>
2024-08-28 14:47:58 -0400

    Run fix

--- src/fyo/src/utils/cmd.ts
+++ src/fyo/src/utils/cmd.ts
@@ -13,22 +13,28 @@ export default class Cmd {
 
     async run() {
         return new Promise( ( resolve, reject ) => {
-            const run = spawn(this.cmd, this.args || []);
-            run.on('error', ( err ) => {
-                console.log( 'Could not start ', this.cmd, err );
-            });
-            
-            run.stdout.on('data', this.stdout || console.log);        
-            run.stderr.on('data', console.error);
-            
-            run.on('close', (code) => {
-              console.log(`child process exited with code ${code}`);
-              if(code == 0) {
-                  resolve( code );
-              } else {
-                  reject( code );
-              }
-            });
+
+            try {
+                const run = spawn(this.cmd, this.args || []);
+                run.on('error', ( err ) => {
+                    console.log( 'Could not start ', this.cmd, err );
+                });
+                
+                run.stdout.on('data', this.stdout || console.log);        
+                run.stderr.on('data', console.error);
+                
+                run.on('close', (code) => {
+                console.log(`child process exited with code ${code}`);
+                if(code == 0) {
+                    resolve( code );
+                } else {
+                    reject( code );
+                }
+                });
+            } catch (e) {
+                console.error(e);
+                reject( e );
+            }
         });
     }
 }
--- src/fyo/src/utils/runApp.ts
+++ src/fyo/src/utils/runApp.ts
@@ -1,6 +1,10 @@
 import cmd from './cmd';
 
 export default function runApp(app) {
-    const result = new cmd('am', ['start', '--user', '0', '-a', 'android.intent.action.MAIN', app + '/.MainActivity']);
-    result.run();
+    try {
+        const result = new cmd('am', ['start', '--user', '0', '-a', 'android.intent.action.MAIN', app + '/.MainActivity']);
+        result.run().catch(console.error);
+    } catch (e) {
+        console.error(e);
+    }
 }
